337034f0ff26ce3f7f0f3e7269f5c383918ccf0e
Mykola Turovskyi <webdev.rds@gmail.com>
2021-03-31 23:19:45 +0500

    Updated brand logos

--- src/themes/capybara/components/omni/om-brand-logos.vue
+++ src/themes/capybara/components/omni/om-brand-logos.vue
@@ -1,53 +1,60 @@
 <template>
-  <div class="om-brand-logos">
-    <div v-for="(logo, index) in logosMock" :key="index" class="logo-wrapper">
-      <SfImage :src="logo" />
-    </div>
-  </div>
+  <SfCarousel
+    class="om-brand-logos"
+    :settings="{
+      animationDuration: 3000,
+      rewind: false,
+      perTouch: true,
+      gap: 0
+    }"
+  >
+    <SfCarouselItem v-for="(logo, index) in logosMock" :key="index">
+      <SfImage :src="logo" class="logo-wrapper" />
+    </SfCarouselItem>
+  </SfCarousel>
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex';
-import {
-  SfImage
-} from '@storefront-ui/vue';
+import { SfImage, SfCarousel } from '@storefront-ui/vue';
 
 export default {
   name: 'OmBrandLogos',
   components: {
-    SfImage
+    SfImage,
+    SfCarousel
   },
   data () {
     return {
       logosMock: new Array(11).fill('assets/brand/Jaguar.png')
-    }
+    };
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 @import "~@storefront-ui/shared/styles/helpers/breakpoints";
 
 .om-brand-logos {
-  display: flex;
-  // flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
   background: white;
-  .logo-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: var(--spacer-xs);
-    border: 1px solid var(--_c-gray-accent-lighten);
+  &.sf-carousel {
+    ::v-deep .sf-carousel__controls {
+      display: none;
+    }
+    ::v-deep .sf-carousel__wrapper {
+      max-width: 100%;
+    }
   }
-}
-
-::v-deep .sf-image img {
-  height: auto !important;
-  width: 100px;
-  transition: transform 0.3s;
-  &:hover {
-    transform: scale(1.2)
+  .sf-carousel-item {
+    text-align: center;
+    // width: 160px !important;
+  }
+  ::v-deep .sf-image img {
+    transition: transform 0.3s;
+    height: 80px;
+    width: auto;
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.2);
+    }
   }
 }
 </style>
