846d027f5b020280fefdf7200d8a45dd5e31e5b0
Mykola Turovskyi <webdev.rds@gmail.com>
2021-04-13 13:00:23 +0500

    Completed svg viewer

--- src/themes/capybara/components/svg-viewer.vue
+++ src/themes/capybara/components/svg-viewer.vue
@@ -1,43 +1,36 @@
 <template>
   <div>
-    <div id="svgTemplate" ref="svgContainer" />
+    <div id="svgTemplate" />
   </div>
 </template>
-
 <script>
 import Vue from 'vue/dist/vue.esm.js';
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const jsdom = require('jsdom');
+const xmlserializer = require('xmlserializer');
 
 export default {
   name: 'SvgViewer',
+  props: {
+    objectId: {
+      type: Number,
+      default: 43
+    }
+  },
   data () {
     return {
       /* Overall situation */
       Svgurl: '', // url of SVG
-      Svgdom: null, // obtained SVG element
+      Svgdom: null // obtained SVG element
       /* Svg variables */
-      photoResult: {
-        Resultval: 0, // test result value
-        Resultmsg: 'not detected', // test result field
-        Resultcolor: 'ාdcdee2' // test result - field background color
-      },
-      objectId: 121
     };
   },
-  // computed: {
-  //   showPart() {
-  //     return this.objectId ===
-  //   }
-  // },
   mounted () {
-    // Bind the takephoto method to the window and provide it for external call
+    this.$nextTick(() => {
+      this.getSvg();
+    })
     window['handleClick'] = () => {
       console.log('hey');
     };
-  },
-  created () {
-    this.getSvg();
   },
   methods: {
     // Initialize svg
@@ -50,70 +43,103 @@ export default {
         true
       );
       xhr.send();
-
       /* Listening to XHR objects */
       xhr.addEventListener('load', () => {
         /* 1. Get dom */
         const parser = new DOMParser();
-        const resXML = parser.parseFromString(
-          xhr.responseText,
-          'application/xml'
-        );
-        const svgContainer = {
-          width: this.$refs['svgContainer'].clientWidth + 'px',
-          height: this.$refs['svgContainer'].clientHeight + 'px'
-        };
 
+        const resXML = parser.parseFromString(xhr.responseText, 'application/xml');
         this.svgDom = resXML.documentElement;
-        this.svgDom.setAttribute('width', '900px');
-        this.svgDom.setAttribute('height', '900px');
-        this.svgDom.setAttribute('viewBox', '0 0 900 900');
-        // this.svgDom.dispatchEvent(new Event("show"));
-        // this.$refs["svgContainer"].dispatchEvent(new Event("show"));
-        /* 2. Modify SVG Object */
 
         let first_g = this.svgDom.getElementById('713500110101');
-        first_g.setAttribute('transform', 'scale(0.6)');
-        // first_g.dispatchEvent(new Event("show"));
-        // first_g.style.transform = `scale(20.2) translate(529px, 221px)`;
-        // first_g.style.transformOrigin = `center`;
         first_g.setAttribute('v-on:click', 'this.handleClick()');
         for (let i = 0; i < first_g.childNodes.length; i++) {
           let g = first_g.childNodes[i];
           if (g.nodeName === 'g') {
-            // g.setAttribute('v-if', `false`);
-            // if (this.objectId == g.getAttribute('id')) {
-            // g.setAttribute('v-if', `true`);
-            g.childNodes.forEach((child_g) => {
-              if (child_g.nodeName === 'g') {
-                child_g.removeAttribute('onclick');
-                child_g.removeAttribute('onmouseover');
-                child_g.removeAttribute('onmouseout');
-                child_g.removeAttribute('transform');
-              }
-            });
-            // }
+            g.setAttribute('v-if', `false`);
+            if (this.objectId == g.getAttribute('id')) {
+              g.setAttribute('v-if', `true`);
+              g.childNodes.forEach((child_g) => {
+                if (child_g.nodeName === 'g') {
+                  child_g.removeAttribute('onclick');
+                  child_g.removeAttribute('onmouseover');
+                  child_g.removeAttribute('onmouseout');
+                  child_g.removeAttribute('transform');
+                }
+              })
+            }
           }
         }
 
-        console.log(first_g.getBBox(), 'hey')
+        this.renderSvg();
+        let bbox = document.querySelector('#svgTemplate svg').getBBox();
+        let bbox_width = bbox.x + bbox.width;
+        this.svgDom.setAttribute('viewBox', '0 0 500 500');
+        this.svgDom.setAttribute('width', '500px');
+        this.svgDom.setAttribute('height', '500px');
+        let scaleTransform = 1;
+        if (bbox.width < 50) {
+          if (bbox.height < 50) {
+            scaleTransform = 5;
+          } else if (bbox.height < 100) {
+            scaleTransform = 4;
+          } else if (bbox.height < 150) {
+            scaleTransform = 3;
+          } else if (bbox.height < 250) {
+            scaleTransform = 2;
+          }
+        } else if (bbox.width < 110) {
+          if (bbox.height < 50) {
+            scaleTransform = 5;
+          } else if (bbox.height < 100) {
+            scaleTransform = 4;
+          } else if (bbox.height < 150) {
+            scaleTransform = 3;
+          } else if (bbox.height < 250) {
+            scaleTransform = 2;
+          }
+        } else if (bbox.width < 190) {
+          if (bbox.height < 100) {
+            scaleTransform = 3;
+          } else if (bbox.height < 250) {
+            scaleTransform = 2;
+          }
+        } else if (bbox.width < 300) {
+          if (bbox.height < 250) {
+            scaleTransform = 2;
+          }
+        }
 
-        /* 3. Modify dom */
-        // this.svgDom.getElementById("713500110101").childNodes[0].nodeValue = ...;
-        // this.svgDom.getElementById("...").setAttribute("style",
-        //   `....; fill:${this.photoResult.resultColor}; ...`);
+        first_g.setAttribute('transform', `scale(` + scaleTransform + ')');
+        this.renderSvg();
+        const bboxNew = document.querySelector('#svgTemplate svg').getBBox();
+        let bbox_x = bboxNew.x;
+        let bbox_y = bboxNew.y;
+        bbox_width = bboxNew.width / 2;
+        let bbox_height = bboxNew.height / 2;
+        let new_x = 250 - bbox_x - bbox_width;
+        let new_y = 250 - bbox_y - bbox_height;
 
-        /* 4. Convert svgdom object into virtual dom of Vue */
-        var oSerializer = new XMLSerializer();
-        var sXML = oSerializer.serializeToString(this.svgDom);
-        var Profile = Vue.extend({
-          template: "<div id='svgTemplate'>" + sXML + '</div>'
-        });
-        // Create an instance and mount it to the element
-        new Profile().$mount('#svgTemplate');
-
-        console.log(first_g.getBBox())
+        first_g.setAttribute(
+          'transform',
+          'translate(' +
+            new_x +
+            ', ' +
+            new_y +
+            ') scale(' +
+            scaleTransform +
+            ')'
+        );
+        this.renderSvg();
       });
+    },
+    renderSvg () {
+      let sXML = xmlserializer.serializeToString(this.svgDom);
+      let Profile = Vue.extend({
+        template: "<div id='svgTemplate'>" + sXML + '</div>'
+      });
+      // Create an instance and mount it to the element
+      new Profile().$mount('#svgTemplate');
     }
   },
   beforeDestroy () {
@@ -129,3 +155,10 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+#svgTemplate {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
